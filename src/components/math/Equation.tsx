3b90422689976bc6c4f99aff540b6247cb10092a
Siddiq <devsfiq@gmail.com>
2023-08-19 15:16:42 +0800

    Fix Equation component causing hydration errors

--- src/components/math/Equation.tsx
+++ src/components/math/Equation.tsx
@@ -1,6 +1,5 @@
+'use client'
 import { MathJax, MathJaxContext } from 'better-react-mathjax'
-import { TypesettingFunction } from 'better-react-mathjax/MathJaxContext/MathJaxContext'
-import { error } from 'console'
 
 interface EquationProps {
   className?: string
@@ -21,13 +20,13 @@ const Equation = ({ text, className }: EquationProps) => {
   if (text === '') text = ' '
 
   return (
-    <div className={className}>
+    <span className={className}>
       <MathJaxContext version={3} config={config}>
         <MathJax inline dynamic>
           {text}
         </MathJax>
       </MathJaxContext>
-    </div>
+    </span>
   )
 }
 
