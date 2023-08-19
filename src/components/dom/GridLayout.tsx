a2706f1d6b2ac7656c4a60f225e9241b6458dc04
Siddiq <devsfiq@gmail.com>
2023-08-19 15:16:12 +0800

    Update GridLayout style

--- src/components/dom/GridLayout.tsx
+++ src/components/dom/GridLayout.tsx
@@ -3,7 +3,7 @@ interface GridLayoutProps {
 }
 
 const GridLayout = ({ children }: GridLayoutProps) => {
-  return <div className='absolute inset-0 top-12 grid grid-cols-content grid-rows-1 gap-4'>{children}</div>
+  return <div className='absolute inset-0 top-12 grid grid-cols-content grid-rows-1 gap-4 bg-slate-50'>{children}</div>
 }
 
 export { GridLayout }
