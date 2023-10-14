f597dee5c3e6ac41d727bb28565bbafca9e6eed4
Siddiq <devsfiq@gmail.com>
2023-10-15 00:30:22 +0800

    Updated Matrix Rotation to new layout

--- app/matrices/rotation/page.tsx
+++ app/matrices/rotation/page.tsx
@@ -1,26 +1,24 @@
 'use client'
 
 import { Loader } from '@/components/Loader'
-import { GridLayout } from '@/components/dom/GridLayout'
 import { KatexEquation } from '@/components/math/Equation'
-import { Box, Environment, Line, OrbitControls, PerspectiveCamera } from '@react-three/drei'
-import { useFrame } from '@react-three/fiber'
+import { PerspectiveCamera } from '@react-three/drei'
 import { useControls } from 'leva'
 import dynamic from 'next/dynamic'
-import { MutableRefObject, Suspense, useEffect, useMemo, useRef } from 'react'
+import { useEffect, useMemo, useRef } from 'react'
 import * as THREE from 'three'
 import { useRotationStore } from './hooks/useRotationStore'
 
+import { Common } from '@/components/canvas/Common'
+import { round } from '@/helpers/math'
+import { Blockquote, Box, Container, Heading, Section, Text } from '@radix-ui/themes'
 import 'katex/dist/katex.min.css'
+import { Cube } from './components/Cube'
 
 const View = dynamic(() => import('@/components/canvas/View').then((mod) => mod.View), {
   ssr: false,
   loading: () => <Loader />,
 })
-
-const round = (float: number): string => {
-  return (Math.round(float * 100) / 100).toFixed(1)
-}
 
 function getMatrix(matrix: THREE.Matrix4) {
   let elements = matrix.elements.map((el) => round(el))
@@ -50,10 +48,6 @@ function getRotationZMatrix(theta?: number) {
   return equation.replaceAll('{theta}', _theta)
 }
 
-interface CubeProps {
-  camera?: MutableRefObject<THREE.PerspectiveCamera>
-  portal?: MutableRefObject<HTMLElement>
-}
 const useRotationControls = () => {
   const [setX, setY, setZ] = useRotationStore((state) => [state.setX, state.setY, state.setZ])
   const { x, y, z } = useControls({
@@ -62,21 +56,21 @@ const useRotationControls = () => {
       step: 0.1,
       min: 0.1,
       max: 360,
-      color: 'red',
+      label: 'x-axis (blue)',
     },
     y: {
       value: 0,
       step: 0.1,
       min: 0.1,
       max: 360,
-      color: 'green',
+      label: 'y-axis (red)',
     },
     z: {
       value: 0,
       step: 0.1,
       min: 0.1,
       max: 360,
-      color: 'blue',
+      label: 'z-axis (green)',
     },
   })
 
@@ -91,64 +85,6 @@ const useRotationControls = () => {
   useEffect(() => {
     setZ(z)
   }, [z, setZ])
-}
-
-const Cube = ({ camera }: CubeProps) => {
-  const rotation = useRotationStore((state) => [state.x, state.y, state.z])
-  const groupRef = useRef<THREE.Group>()
-
-  const speed = 15
-  const newRotation = new THREE.Quaternion(...rotation)
-
-  const radians = useMemo(() => {
-    const toRad = (deg: number) => (deg * Math.PI) / 180
-    return rotation.map((deg) => toRad(deg))
-  }, [rotation])
-
-  useFrame((state, delta) => {
-    newRotation.setFromEuler(new THREE.Euler(...radians))
-    groupRef.current.quaternion.slerp(newRotation, delta * speed)
-  })
-
-  return (
-    <>
-      <group ref={groupRef}>
-        <Box args={[5, 5, 5]}>
-          <meshStandardMaterial color={'orange'} />
-        </Box>
-      </group>
-    </>
-  )
-}
-
-const SimpleGrid = () => {
-  return (
-    <>
-      <group>
-        <Line
-          points={[
-            [-20, 0, 0],
-            [20, 0, 0],
-          ]}
-          color='red'
-        />
-        <Line
-          points={[
-            [0, -20, 0],
-            [0, 20, 0],
-          ]}
-          color='green'
-        />
-        <Line
-          points={[
-            [0, 0, -20],
-            [0, 0, 20],
-          ]}
-          color='blue'
-        />
-      </group>
-    </>
-  )
 }
 
 export default function MatrixRotationPage() {
@@ -175,58 +111,46 @@ export default function MatrixRotationPage() {
 
   return (
     <>
-      <GridLayout>
-        <Suspense fallback={null}>
-          <div className='flex flex-col gap-4 overflow-auto bg-white px-12 py-8'>
-            <h2 className='text-center text-4xl font-semibold'>Rotation</h2>
-            <p>
-              A Rotation Matrix allows you to rotate objects in 3D space. It is typically composed of a series of 4x4
-              matrices, each representing a rotation around a particular axis.
-            </p>
-            <div className='flex flex-col gap-2'>
-              <p className='text-center'>
-                <KatexEquation className='block' text={`R_{x} = ${getRotationXMatrix()}`}></KatexEquation>
-              </p>
-              <p className='text-center'>
-                <KatexEquation className='block' text={`R_{y} = ${getRotationYMatrix()}`}></KatexEquation>
-              </p>
-              <p className='text-center'>
-                <KatexEquation className='block' text={`R_{z} = ${getRotationZMatrix()}`}></KatexEquation>
-              </p>
-              <p className='italic'>Note that the values represented are in radians</p>
-            </div>
-            <div>
-              <p>
-                The respective axis are then multiplied in order to get the desired rotation matrix that results in what
-                you are seeing now
-              </p>
-              <div className='m-4 mx-auto flex rounded-md bg-white p-4'>
-                <KatexEquation
-                  className='block'
-                  text={`${getRotationXMatrix(radians[0])}${getRotationYMatrix(radians[1])}${getRotationZMatrix(
-                    radians[2],
-                  )}`}
-                ></KatexEquation>
-              </div>
-            </div>
-            <p>The final rotation matrix after multiplying all the axis is as follows</p>
-            <KatexEquation className='block' text={finalRotation}></KatexEquation>
-          </div>
-        </Suspense>
-        <div className='bg-zinc-800'>
-          <div className='relative' ref={view1} />
-          <View className='h-full w-full' index={0} track={view1} orbit>
-            <Cube />
-            <Environment preset='city' />
-            <SimpleGrid />
-            <ambientLight intensity={0.3} />
-            <pointLight position={[5, 5, 5]} intensity={0.8} />
-            <directionalLight position={[0, 0, 10]} intensity={0.3} />
-            <PerspectiveCamera ref={cameraRef} makeDefault position={[10, 10, 10]} fov={60} />
-            <OrbitControls makeDefault />
-          </View>
-        </div>
-      </GridLayout>
+      <div className='relative' ref={view1} />
+      <View className='h-full w-full' index={0} track={view1} orbit>
+        <Cube />
+        <Common />
+        <PerspectiveCamera ref={cameraRef} makeDefault position={[15, 15, 15]} fov={60} />
+      </View>
+      <div className='fixed pl-80 w-screen left-0 top-10 z-10'>
+        <Container p='4'>
+          <Heading size='4' weight='light'>
+            A Rotation Matrix is a 4x4 matrix used in 3D graphics to represent the orientation of an object in space.<br />
+            It is commonly composed of a series of 4x4 matrices, with each matrix representing a rotation around a specific axis.<br />
+          </Heading>
+          <Box mt={'2'}>
+            <Text size={'2'}>Note that the values are represented in radians</Text>
+          </Box>
+          <Section size='1' mt={'2'} className='flex justify-center items-center'>
+            <Box className='text-blue-400'>
+              <KatexEquation text={getRotationXMatrix(radians[0])} />
+              <p className='text-center'><KatexEquation text={'\\text{x-axis}'} /></p>
+            </Box>
+            <Box className='text-red-400'>
+              <KatexEquation text={getRotationYMatrix(radians[1])} />
+              <p className='text-center'><KatexEquation text={'\\text{y-axis}'} /></p>
+            </Box>
+            <Box className='text-green-400'>
+              <KatexEquation text={getRotationZMatrix(radians[2])} />
+              <p className='text-center'><KatexEquation text={'\\text{z-axis}'} /></p>
+            </Box>
+            <Box className=''>
+              <KatexEquation text={`= ${finalRotation}`}></KatexEquation>
+              <p className='text-center'><KatexEquation className='ml-5' text={'\\text{ final rotation}'} /></p>
+            </Box>
+          </Section>
+        </Container>
+      </div>
+      <div className='fixed pl-80 w-screen left-0 bottom-10 z-10'>
+        <Section p='6'>
+          <Blockquote>Explore the impact of different rotation values on each axis, observing how they dynamically influence the object's orientation in 3D space.</Blockquote>
+        </Section>
+      </div>
     </>
   )
 }
