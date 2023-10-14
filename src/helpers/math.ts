0702914c326327cbe1096339e0e4a08e60d3e5ad
Siddiq <devsfiq@gmail.com>
2023-10-15 00:56:53 +0800

    Updated missing math functions

--- src/helpers/math.ts
+++ src/helpers/math.ts
@@ -2,4 +2,14 @@ const round = (float: number): string => {
   return Math.round(float).toFixed(1)
 }
 
-export { round }
+const normalise = (vector: [number, number, number]): [number, number, number] => {
+  const [x, y, z] = vector
+  const length = Math.sqrt(x * x + y * y + z * z)
+
+  if (length == 0) return [0, 0, 0]
+  return [x / length, y / length, z / length]
+}
+
+const radians = (degree: number) => (degree * Math.PI) / 180
+
+export { round, normalise, radians }
