54fa1309e4b0e79f2e61348d290bf748eaa4679d
Siddiq <devsfiq@gmail.com>
2023-10-15 00:30:31 +0800

    Updated Matrix Scaling to new layout

--- app/matrices/scaling/page.tsx
+++ app/matrices/scaling/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { Loader } from '@/components/Loader'
-import { GridLayout } from '@/components/dom/GridLayout'
+import { GridLayout } from '@/components/layout/GridLayout'
 import { KatexEquation } from '@/components/math/Equation'
 import {
   Box,
@@ -22,6 +22,9 @@ import * as THREE from 'three'
 import { useScaleStore } from './hooks/useScaleStore'
 
 import 'katex/dist/katex.min.css'
+import { Common } from '@/components/canvas/Common'
+import { Cube } from './components/Cube'
+import { Blockquote, Container, Heading, Section } from '@radix-ui/themes'
 
 const View = dynamic(() => import('@/components/canvas/View').then((mod) => mod.View), {
   ssr: false,
@@ -58,76 +61,6 @@ const useScaleControls = () => {
   }, [scale, setScale])
 }
 
-const Arrow = ({ origin, direction, length }: ArrowProps) => {
-  const arrowRef = useRef<THREE.ArrowHelper>()
-
-  useEffect(() => {
-    arrowRef.current.position.set(...origin)
-    arrowRef.current.setDirection(new THREE.Vector3(...direction))
-    arrowRef.current.setLength(length)
-  }, [origin, direction, length])
-
-  return (
-    <>
-      <arrowHelper ref={arrowRef}></arrowHelper>
-    </>
-  )
-}
-
-const Cube = ({ camera }: CubeProps) => {
-  const scale = useScaleStore((state) => state.scale)
-  const groupRef = useRef<THREE.Group>()
-  const lineRef = useRef<THREE.Line>()
-
-  const speed = 15
-  const newScale = new THREE.Vector3()
-
-  useFrame((state, delta) => {
-    newScale.set(scale[0], scale[1], scale[2])
-    groupRef.current.scale.lerp(newScale, delta * speed)
-  })
-
-  return (
-    <>
-      <group ref={groupRef}>
-        <Box args={[1, 1, 1]}>
-          <meshStandardMaterial color={'orange'} />
-        </Box>
-      </group>
-    </>
-  )
-}
-
-const SimpleGrid = () => {
-  return (
-    <>
-      <group>
-        <Line
-          points={[
-            [-20, 0, 0],
-            [20, 0, 0],
-          ]}
-          color='red'
-        />
-        <Line
-          points={[
-            [0, -20, 0],
-            [0, 20, 0],
-          ]}
-          color='green'
-        />
-        <Line
-          points={[
-            [0, 0, -20],
-            [0, 0, 20],
-          ]}
-          color='blue'
-        />
-      </group>
-    </>
-  )
-}
-
 export default function MatrixScalingPage() {
   const cameraRef = useRef<THREE.PerspectiveCamera>()
 
@@ -140,7 +73,7 @@ export default function MatrixScalingPage() {
 
   return (
     <>
-      <GridLayout>
+      {/* <GridLayout>
         <Suspense fallback={null}>
           <div className='flex flex-col gap-4 bg-white px-12 py-8'>
             <h2 className='text-center text-4xl font-semibold'>Scaling</h2>
@@ -170,20 +103,32 @@ export default function MatrixScalingPage() {
             </div>
           </div>
         </Suspense>
-        <div className='bg-zinc-800'>
-          <div className='relative' ref={view1} />
-          <View className='h-full w-full' index={0} track={view1} orbit>
-            <Cube />
-            <Environment preset='city' />
-            <SimpleGrid />
-            <ambientLight intensity={0.3} />
-            <pointLight position={[5, 5, 5]} intensity={0.8} />
-            <directionalLight position={[0, 0, 10]} intensity={0.3} />
-            <PerspectiveCamera ref={cameraRef} makeDefault position={[10, 10, 10]} fov={60} />
-            <OrbitControls makeDefault />
-          </View>
-        </div>
-      </GridLayout>
+      </GridLayout> */}
+      <div className='relative' ref={view1} />
+      <View className='h-full w-full' index={0} track={view1} orbit>
+        <Cube />
+        <Common />
+        <PerspectiveCamera ref={cameraRef} makeDefault position={[15, 15, 15]} fov={60} />
+      </View>
+      <div className='fixed pl-80 w-screen left-0 top-10 z-10'>
+        <Container p='4'>
+          <Heading size='4' weight='light'>
+            A Scale Matrix is a 4x4 matrix used in 3D graphics to control the size or scale of an object along each axis.<br />
+            It is an essential component for resizing objects in a three-dimensional space.
+          </Heading>
+          <Section size='1' mt={'2'} className='flex justify-center items-center'>
+            <KatexEquation
+              className='mx-auto block'
+              text={`p^{\\prime} = \\begin{bmatrix} x^{\\prime} \\\\ y^{\\prime} \\\\ z^{\\prime} \\\\ 1 \\end{bmatrix} = \\begin{bmatrix} ${_scale[0]} & 0 & 0 & 0 \\\\ 0 & ${_scale[1]} & 0 & 0 \\\\ 0 & 0 & ${_scale[2]} & 0 \\\\ 0 & 0 & 0 & 1 \\end{bmatrix} \\begin{bmatrix} x \\\\ y \\\\ z \\\\ 1 \\end{bmatrix} = \\begin{bmatrix} x \\cdot t_x \\\\ y \\cdot t_y \\\\ z \\cdot t_z \\\\ 1 \\end{bmatrix}`}
+            />
+          </Section>
+        </Container>
+      </div>
+      <div className='fixed pl-80 w-screen left-0 bottom-10 z-10'>
+        <Section p='6'>
+          <Blockquote>Explore the effects of different scaling values on each axis to see how the Scale Matrix dynamically adjusts the object's size in 3D space.</Blockquote>
+        </Section>
+      </div>
     </>
   )
 }
